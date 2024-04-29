ea46376a9db7d6bda1bf5ea3bc965d5ec7bc7ef6
Max Schultz <maxjs@bu.edu>
2024-04-29 14:47:31 -0400

    Modified BFS Polished and Complete

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,23 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::collections::VecDeque;
 
 fn main() {
     println!("Hello, world!");
     let data = read_data("facebook_combined.txt");
-    let deg_cent = degree_centrality(&data, 24);
+    //let deg_cent = degree_centrality(&data, 24);
     //println!("Degree Centrality: {:?}", deg_cent);
-
+    let deg = modified_bfs(&data, 4);
+    println!("{:?}", deg);
+    //println!("{:?}", data);
 }
 
-fn read_data(path: &str) -> HashMap<usize, usize> {
-    let file = File::open(path).expect("Cannot Open File"); 
+fn read_data(path: &str) -> HashMap<usize, Vec<usize>> {
+    let file = File::open(path).expect("Cannot Open File");
     let reader = BufReader::new(file);
     let mut lines = reader.lines();
-    let mut graph: HashMap<usize,usize> = HashMap::new();
+    let mut graph: HashMap<usize, Vec<usize>> = HashMap::new();
     if let Some(Ok(first_line)) = lines.next() {
         for line in lines {
             if let Ok(line_str) = line {
@@ -23,21 +26,23 @@ fn read_data(path: &str) -> HashMap<usize, usize> {
                 if let (Some(u_str), Some(v_str)) = (iter.next(), iter.next()) {
                     let u = u_str.parse::<usize>().expect("Failed to parse integer");
                     let v = v_str.parse::<usize>().expect("Failed to parse integer");
-                    graph.insert(u, v);
+                    graph.entry(u).or_insert_with(Vec::new).push(v);
+                    graph.entry(v).or_insert_with(Vec::new).push(u);
                 }
             }
         }
     }
     return graph;
 }
-fn degree_centrality(graph: &HashMap<usize,usize> , starting_node: usize) -> f64 {
+
+fn degree_centrality(graph: &HashMap<usize, usize> , start: usize) -> f64 {
     let num_nodes = graph.len();
-    if !graph.contains_key(&starting_node) {
+    if !graph.contains_key(&start) {
     panic!("Starting node not found in the graph");
     }
-    let neighbors = match graph.get(&starting_node) {
+    let neighbors = match graph.get(&start) {
       Some(neighbor_list) => neighbor_list,
-      None => return 0.0, // No neighbors, centrality is 0
+      None => return 0.0, // event that there are no neighbors, the centrality is 0
     };
 
     let degree = (*neighbors as f64).ln() as f64;
@@ -45,31 +50,31 @@ fn degree_centrality(graph: &HashMap<usize,usize> , starting_node: usize) -> f64
     println!("Degree Centrality: {}", degree);
     return degree; 
 }
-/*fn bfs(graph: HashMap<u32,u32>, node_id: u32) -> HashMap<u32,u32> {
 
+fn modified_bfs(graph: &HashMap<usize, Vec<usize>>, starting_node: usize) -> Vec<(usize, u32)> {
+    let mut distance: Vec<Option<u32>> = vec![None; graph.len()];
+    distance[starting_node] = Some(0); // <= we know this distance
+    let mut queue: VecDeque<usize> = VecDeque::new();
+    queue.push_back(starting_node);
+    let mut result: HashMap<usize, u32> = HashMap::new();
+    while let Some(v) = queue.pop_front() {
+        if let Some(neighbors) = graph.get(&v) {
+            for &u in neighbors.iter() {
+                if distance[u].is_none() {
+                    distance[u] = Some(distance[v].unwrap() + 1);
+                    queue.push_back(u);
+                    result.insert(u, distance[u].unwrap());
+                }
+            }
+        }
+    }
+    let mut sorted_result: Vec<(usize, u32)> = result.into_iter().collect();
+    sorted_result.sort_by(|a, b| a.1.cmp(&b.1));
+    return sorted_result;
 }
-fn recommend_friends(graoh: HashMap<u32,u32>, node_id: u32, max_depth: u32) -> Vec<(u32,u32)> {
 
-}*/
+
 /* 
-Pseudocode:
-
-read_data()
-- reads the data from a file path 
-- parses txt file line-by-line and extracts the node ids, etc.
-- returns and creates the adjacency list — adding the users and their connections
-
-degree_centrality()
-- takes the adjacency list and the node id as arguments
-- calculates the degree centrality for the user — which is defined as the number of connections
-- should return the degree centrality as f32?
-
-bfs()
-- takes the graph/ adjacency list and node id as input
-- implements BFS from the starting user
-- keep track of degree separation for each user
-- return hashmap where key is the node id and the value is the degree separation
-
 recommend_friends()
 - takes the adjacency list, node id, and max_depth as inputs/ arguments
 - calls bfs function to find closely related users for the specified max_depth
